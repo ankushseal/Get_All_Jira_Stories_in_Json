4d5a9dafdd1a05e8678f9b9b47adeac0e7f4a107
ankushseal <65338558+ankushseal@users.noreply.github.com>
2024-02-26 00:50:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://testing500.atlassian.net/rest/api/3/issue/TES-2"
+url = "your jira board url"
 
-auth = HTTPBasicAuth("25092000ankushseal@gmail.com", "ATATT3xFfGF0JX-nib0WoBch7r7sCZ29xX8P0HqJXd4uuub3bTkpknpu9ggS90FPNwok0FJttq3s3KOdWY2NJM9yKr4ZiPuaqfoIMpMN3zug4pq3lsPDvHInrQiZ_77nSLXlWu7mIQbiT-TuGKnKkiBUbMu9_AURZPXOTHwKdFlMyvo-PU-cFMk=B7D377E7")
+auth = HTTPBasicAuth("Jira's login id", "jira API Key")
 
 headers = {
   "Accept": "application/json"
